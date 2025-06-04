391bb0e2f1ea4a31b0f58cfabbebeeecb3d7b860
Mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2025-06-03 22:09:09 -0300

    Feat: Atualização

---
+++ 02/05_modulos_externos/index.js
@@ -0,0 +1,14 @@
+const minimist = require('minimist');
+
+const args = minimist(process.argv.slice(2));
+
+console.log(args);
+
+const nome = args["nome"];
+console.log(`Meu nome é ${nome}!`);
+
+const idade = args["idade"];
+console.log(`Minha idade é ${idade}!`);
+
+const profissao = args["profissao"];
+console.log(`Minha profissão é ${profissao}!`);
